48aa3c8a47c0a0b7404cf2a0cc96a8cd6aeae487
yenchen <q123wer2002@gmail.com>
2015-02-19 17:03:20 +0800

    search jobs

--- jobs/joblist.php
+++ jobs/joblist.php
@@ -12,6 +12,9 @@
 
 	$indeedHTML= file_get_html($url);
 
+	//savejobs
+	$results=array();
+
 	//echo $indeedHTML;
 
 	//職缺名稱：
@@ -20,34 +23,35 @@
 	//職缺來源：
 
 	$jobdiv=0;
-	while(!empty($indeedHTML->find('div[class=result]',$jobdiv+1)))
+	while(!empty($indeedHTML->find('div[class=result]',$jobdiv)))
 	{
 		$jobDivs=$indeedHTML->find('div[class=result]',$jobdiv);
 		//echo $jobDivs;
 		if($jobDivs->find('h2[class=jobtitle]',0)->plaintext != ""){
 			//公司Logo
+			
+			//網址連結
 			$jobHref="http://tw.indeed.com".$jobDivs->find('h2[class=jobtitle]',0)->find('a',0)->href;
-			echo "<a href='".$jobHref."' style='color:#000000;'><div style='text-align:center;'>";
-
 			//職缺名稱
 			$jobtitle=$jobDivs->find('h2[class=jobtitle]',0)->plaintext;
-			echo "職缺名稱：".$jobtitle."<BR>";
 			//職缺公司
 			$jobCompany=$jobDivs->find('span[class=company]',0)->plaintext;
-			echo "職缺公司：".$jobCompany."<BR>";
 			//職缺地點
 			$jobLocation=$jobDivs->find('span[class=location]',0)->plaintext;
-			echo "職缺地點：".$jobLocation."<BR>";
 			//職缺來源
 			$jobSource=$jobDivs->find('span[class=sdn]',0)->plaintext;
-				if($jobSource==""){echo "職缺來源：Indeed<BR>";}
-				else{echo "職缺來源：".$jobSource."<BR>";}
-			echo "</div></a>";
+			if($jobSource==""){$jobSource="Indeed";}
 
-			echo "<br><hr>";
+			$results[$jobdiv]=array();
+			$results[$jobdiv]['src']=$jobHref;
+			$results[$jobdiv]['title']=$jobtitle;
+			$results[$jobdiv]['company']=$jobCompany;
+			$results[$jobdiv]['location']=$jobLocation;
+			$results[$jobdiv]['source']=$jobSource;
 		}
 		$jobdiv++;
 	}
+	print_r($results);
 
 /*
 	$jobDivs=$indeedHTML->find('div[class=result]',2);
